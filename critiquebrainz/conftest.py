f4ab709153837b2edf12e741ad2da3f6f11e3757
Suyash Garg <suyashgargsfam@gmail.com>
2017-07-26 17:18:22 +0530

    Add docstrings for functions in conftest.py and utils.py

--- critiquebrainz/conftest.py
+++ critiquebrainz/conftest.py
@@ -1,3 +1,6 @@
+"""
+This module defines pytest fixtures and hooks which are used for configuring the test environment.
+"""
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
@@ -10,6 +13,8 @@ TEST_DB_URI = 'postgres://musicbrainz:musicbrainz@musicbrainz_test_db:5432/music
 
 @pytest.fixture(scope="session", autouse=True)
 def populate_mb_database(request):
+    """Populate MusicBrainz test db with the test data at start of a test session
+    and remove the added test data after the test session completes."""
     mb.engine = create_engine(TEST_DB_URI, poolclass=NullPool)
     populate_db()
     def empty_mb_database():
@@ -19,5 +24,5 @@ def populate_mb_database(request):
 
 def pytest_keyboard_interrupt(excinfo):
     #pylint: disable=unused-argument
-    """Called for keyboard interrupt."""
+    """Remove test data from the test db in case of a keyboard interrupt."""
     remove_test_data()
--- critiquebrainz/frontend/external/musicbrainz_db/utils.py
+++ critiquebrainz/frontend/external/musicbrainz_db/utils.py
@@ -4,7 +4,9 @@ import critiquebrainz.frontend.external.musicbrainz_db as mb
 ADMIN_SQL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', '..', '..', 'admin', 'sql')
 
 def populate_db():
+    """Populate test database with the test data."""
     mb.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_test_data.sql'))
 
 def remove_test_data():
+    """Remove test data from the test database."""
     mb.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'delete_test_data.sql'))
